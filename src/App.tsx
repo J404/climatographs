ab06c897ee358f4e6f1448cc56139f25950e6ce3
Jimmy <jimmystry@gmail.com>
2021-03-30 17:29:43 -0400

    Proof of concept; will delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface APIResponse {
+  monthVals: [number];
+}
+
 function App() {
+  const [data, updateData] = useState([] as unknown as [APIResponse]);
+  
+  useEffect(() => {
+    async function callAPI() {
+      const response = await fetch('http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/pr/1980/1999/usa');
+      const climateData: [APIResponse] = await response.json();
+
+      updateData(climateData);
+    }
+
+    callAPI();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +26,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Weather data:</p>
+        {
+          data.map(item => {
+            return item.monthVals.map(monthval => {
+              return <span>{monthval}</span>
+            })
+          })
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
