a4e882ec71a51f2812e5142500121ef64ba6c814
Jimmy <jimmystry@gmail.com>
2021-04-01 22:02:01 -0400

    Decided to lump precip and temp data into a single function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import { ClimateData, getPrecipData } from "./climateAPI";
+import { ClimateData, getClimateData } from "./climateAPI";
 
 function App() {
   return (
     <div className="App">
       <div className="">Hello!</div>
       <button
-        onClick={async () => console.log(await getPrecipData(1980, 1999, "usa"))}
+        onClick={async () => console.log(await getClimateData(1980, 1999, "usa"))}
       >Click</button>
     </div>
   );
--- src/climateAPI.ts
+++ src/climateAPI.ts
@@ -1,3 +1,5 @@
+type APIResponse = MonthlyData[] | AnnualData[];
+
 interface Data {
   gcm: string;
   variable: string;
@@ -13,34 +15,58 @@ interface AnnualData extends Data {
   annualData: number[];
 }
 
-export interface ClimateData extends Data {
+interface CompleteData extends Data {
   monthVals: number[];
   annualData: number;
 }
 
-export const getPrecipData = async (
+export interface ClimateData {
+  precipData: CompleteData;
+  tempData: CompleteData;
+}
+
+const getData = async (
+  interval: string,
+  type: string,
+  start: number,
+  end: number,
+  country: string
+): Promise<APIResponse> => {
+  const response = await fetch(
+    `http://climatedataapi.worldbank.org/climateweb/rest/v1/country/${interval}/${type}/${start}/${end}/${country}`
+  );
+
+  const data: APIResponse = ((await (
+    await response
+  ).json()) as unknown) as APIResponse;
+
+  return data;
+};
+
+export const getClimateData = async (
   start: number,
   end: number,
   country: string
 ): Promise<ClimateData> => {
-  const monthlyResponse = await fetch(
-    `http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/pr/${start}/${end}/${country}`
-  );
-
-  const monthlyData: MonthlyData[] = await ((
-    await monthlyResponse
-  ).json() as unknown) as MonthlyData[];
-
-  const annualResponse = await fetch(
-    `http://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/pr/${start}/${end}/${country}`
-  );
-
-  const annualData: AnnualData[] = await ((
-    await annualResponse
-  ).json() as unknown) as AnnualData[];
+  const monthlyPrecip: MonthlyData[] = 
+    await getData('mavg', 'pr', 1980, 1999, 'usa') as MonthlyData[];
+  const annualPrecip: AnnualData[] = 
+    await getData('annualavg', 'pr', 1980, 1999, 'usa') as AnnualData[];
   
-  return {
-    ...monthlyData[0],
-    annualData: annualData[0].annualData[0],
+  const precipData: CompleteData = {
+    ...monthlyPrecip[0],
+    annualData: annualPrecip[0].annualData[0],
   };
+
+  const monthlyTemp: MonthlyData[] = 
+    await getData('mavg', 'pr', 1980, 1999, 'usa') as MonthlyData[];
+  const annualTemp: AnnualData[] = 
+    await getData('annualavg', 'pr', 1980, 1999, 'usa') as AnnualData[];
+  
+  const tempData: CompleteData = {
+    ...monthlyTemp[0],
+    annualData: annualTemp[0].annualData[0],
+  }
+
+  return { precipData, tempData };
 };
