009eb899d98ce3892f77575a3ee219ab0f0bf4d3
Jimmy <jimmystry@gmail.com>
2021-04-01 22:53:37 -0400

    Different yaxes

--- src/climatograph.ts
+++ src/climatograph.ts
@@ -1,24 +1,54 @@
-import { Chart } from "chart.js";
+import { Chart } from 'chart.js';
 
 export const makeChart = async (
   ctx: CanvasRenderingContext2D,
   precipData: number[],
-  tempData: number[],
-  ) => {
+  tempData: number[]
+) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      datasets: [{
-        label: 'Monthly Average Precipitation (mm)',
-        data: precipData,
-      }, {
-        type: 'line',
-        label: 'Monthly Average Temperature (C)',
-        data: tempData,
-      }],
-      labels: ['J', 'F', 'M', 'A', 'M', 'J', 
-      'J', 'A', 'S', 'O', 'N', 'D']
-    }
+      datasets: [
+        {
+          label: 'Monthly Average Precipitation (mm)',
+          yAxisID: 'P',
+          data: precipData,
+        },
+        {
+          type: 'line',
+          label: 'Monthly Average Temperature (C)',
+          yAxisID: 'T',
+          data: tempData,
+        },
+      ],
+      labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            id: 'P',
+            type: 'linear',
+            position: 'left',
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 100,
+              stepSize: 10,
+            },
+          },
+          {
+            id: 'T',
+            type: 'linear',
+            position: 'right',
+            ticks: {
+              suggestedMin: -15,
+              suggestedMax: 25,
+              stepSize: 5,
+            },
+          },
+        ],
+      },
+    },
   });
-}
+};
