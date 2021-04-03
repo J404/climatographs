74502d215de74c09ed35994de2709b2d0da3f061
Jimmy <jimmystry@gmail.com>
2021-04-02 20:51:14 -0400

    Loading symbol

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   const [climateData, setClimateData] = useState<ClimateData>(
     ({} as unknown) as ClimateData
   );
+  const [loading, setLoading] = useState(false);
   const chartCanvas = useRef<HTMLCanvasElement>(null);
   const countrySelect = useRef<HTMLSelectElement>(null);
 
@@ -26,12 +27,18 @@ function App() {
   }, [climateData]);
 
   const updateData = async () => {
-    const country = (countrySelect.current as unknown as HTMLSelectElement)
+    if (loading) return;
+
+    const country = ((countrySelect.current as unknown) as HTMLSelectElement)
       .value;
     const iso3 = countryCodes[country];
 
+    setLoading(true);
+
     const newData = await getClimateData(1980, 1999, iso3);
     setClimateData(newData);
+
+    setLoading(false);
   };
 
   const showChart = (precipData: number[], tempData: number[]) => {
@@ -44,31 +51,31 @@ function App() {
 
   return (
     <div className="App">
-      <h2 className='text-5xl p-8'
-      >Climatograph Generator</h2>
-
+      <h2 className="text-5xl p-8">Climatograph Generator</h2>
       Pick the country:
       <br></br>
-
-      <select className='bg-gray-800 rounded-md p-2 my-4'
-      ref={countrySelect}>
-        {
-          Object.keys(countryCodes)
-          .map((country, i) => 
-            <option key={i}>{country}</option>
-          )
-        }
+      <select className="bg-gray-800 rounded-md p-2 my-4" ref={countrySelect}>
+        {Object.keys(countryCodes).map((country, i) => (
+          <option key={i}>{country}</option>
+        ))}
       </select>
-
       <br></br>
-
-      <button 
-      className={'border-gray-800 border-2 p-3 rounded-md ' +
-      'hover:bg-gray-800 transition ease-in-out'}
-      onClick={async () => await updateData()}
-      >Generate Climatograph</button>
-
-      <div className='w-3/4 mx-auto my-4 overflow-x-auto'>
+      <button
+        className={
+          'border-gray-800 border-2 p-3 rounded-md ' +
+          'hover:bg-gray-800 transition ease-in-out'
+        }
+        onClick={async () => await updateData()}
+      >
+        Generate Climatograph
+      </button>
+      <br></br>
+      {loading ? (
+        <div className="animate-spin text-3xl mt-2 w-4 mx-auto">🌩️</div>
+      ) : (
+        <div className='text-3xl mt-2 h-7 w-4 mx-auto'></div>
+      )}
+      <div className="w-3/4 mx-auto my-4 overflow-x-auto">
         <canvas ref={chartCanvas}></canvas>
       </div>
     </div>
